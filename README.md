72f67ca718759b3e281a09c09ae440aa94b94929
emma-elyon <emma-elyon@users.noreply.github.com>
2023-11-18 14:24:57 +0100

    Add initial console prompt

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Kitty24
 
-Eventually compiler, assembler, virtual machine, and web front-end for Kitty24,
+Eventually compiler, assembler, virtual machine, and front-end for Kitty24,
 a fantasy emulator.
--- kitty24_console/src/main.rs
+++ kitty24_console/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::create_dir_all;
 
 use dirs;
+use dialoguer;
 
 fn main() {
 	let audio_processor_js = include_str!("../../web/audio-processor.js");
@@ -12,6 +13,10 @@ fn main() {
 	let root_folder = documents.join("Kitty24");
 	let projects_folder = root_folder.join("projects");
 	let saves_folder = root_folder.join("saves");
+
+	println!("Kitty24 0.1.0");
+
+	// Create projects folder if it does not exist.
 	match projects_folder.try_exists() {
 		Ok(false) => create_dir_all(projects_folder.clone()).expect("Could not create projects folder."),
 		Err(error) => panic!("{}", error),
@@ -19,11 +24,10 @@ fn main() {
 	}
 	match projects_folder.read_dir() {
 		Ok(directory) => {
-			for entry in directory {
-				if let Ok(entry) = entry {
-					println!("{}", entry.file_name().to_str().unwrap_or("INVALID"));
-				}
-			}
+			let entries: Vec<_> = directory.map(|entry| entry.unwrap().file_name().to_str().unwrap_or("INVALID NAME").to_string()).collect();
+			let entries = entries.as_slice();
+			let project_selection = dialoguer::Select::new().default(0).with_prompt("Project").items(entries).interact().unwrap();
+			let command_selection = dialoguer::Select::new().default(0).with_prompt("Command").items(&["build", "debug", "run", "open"]).interact().unwrap();
 		},
 		Err(error) => panic!("{}", error),
 	}
